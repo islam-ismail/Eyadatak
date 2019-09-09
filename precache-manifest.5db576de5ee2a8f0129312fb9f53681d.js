7f5ad790cb7f7049b6707ed5de6c109f467b8fc9
ramy ali <ramy.ali-elbasyouni@vodafone.com>
2019-09-10 01:35:08 +0200

    Updates

---
+++ precache-manifest.5db576de5ee2a8f0129312fb9f53681d.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf4e1370146d389738d532fcd2de572",
+    "url": "/eyadatak/index.html"
+  },
+  {
+    "revision": "2bd252c1212f1b6762df",
+    "url": "/eyadatak/static/css/2.b1119605.chunk.css"
+  },
+  {
+    "revision": "7684a43e1bf8ac99da1e",
+    "url": "/eyadatak/static/css/main.9cdcb931.chunk.css"
+  },
+  {
+    "revision": "2bd252c1212f1b6762df",
+    "url": "/eyadatak/static/js/2.96c5832a.chunk.js"
+  },
+  {
+    "revision": "7684a43e1bf8ac99da1e",
+    "url": "/eyadatak/static/js/main.8c293ed0.chunk.js"
+  },
+  {
+    "revision": "62d90c24d638a5aa5350",
+    "url": "/eyadatak/static/js/runtime~main.a1ed4fbb.js"
+  },
+  {
+    "revision": "0388cf06a8bc7ac222653aa0dd0dd9d6",
+    "url": "/eyadatak/static/media/Droid-Arabic-Kufi.ttf.0388cf06.svg"
+  },
+  {
+    "revision": "56fd25564c068faf10b47d83fcda55b0",
+    "url": "/eyadatak/static/media/Droid-Arabic-Kufi.ttf.56fd2556.woff"
+  },
+  {
+    "revision": "7bbc002a4bda2701ec30721ce02eb366",
+    "url": "/eyadatak/static/media/Droid-Arabic-Kufi.ttf.7bbc002a.eot"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/eyadatak/static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/eyadatak/static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/eyadatak/static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "daf0a656ce5a68a31dc9f853eb89dcfd",
+    "url": "/eyadatak/static/media/attach.daf0a656.svg"
+  },
+  {
+    "revision": "552fdf2d86cbad47a0ade7210c2b79dd",
+    "url": "/eyadatak/static/media/blue-shape.552fdf2d.svg"
+  },
+  {
+    "revision": "0eb60f1c6dfc25713ab01b386a01a9ca",
+    "url": "/eyadatak/static/media/close.0eb60f1c.svg"
+  },
+  {
+    "revision": "7d2ab9c536b64f878422139b5416a923",
+    "url": "/eyadatak/static/media/discuss-icon.7d2ab9c5.svg"
+  },
+  {
+    "revision": "d51c4cbdf58ed633c094cc393e644e66",
+    "url": "/eyadatak/static/media/down-arrow.d51c4cbd.svg"
+  },
+  {
+    "revision": "222b68202e41412f20ada5ee3b5a5258",
+    "url": "/eyadatak/static/media/droidnaskh-bold.222b6820.woff2"
+  },
+  {
+    "revision": "d7a4badf0b4c56ed6646b99f1481362d",
+    "url": "/eyadatak/static/media/droidnaskh-bold.d7a4badf.woff"
+  },
+  {
+    "revision": "0722e192fc7a7bfad4635a0736454eba",
+    "url": "/eyadatak/static/media/droidnaskh-regular.0722e192.woff"
+  },
+  {
+    "revision": "30d191babb45480e65b9491afbbf8f25",
+    "url": "/eyadatak/static/media/droidnaskh-regular.30d191ba.woff2"
+  },
+  {
+    "revision": "2dc1a3b1b25df523ac780656b01f1dee",
+    "url": "/eyadatak/static/media/dropdown-arrow.2dc1a3b1.svg"
+  },
+  {
+    "revision": "bee7ecf03a6b2e8538181c710e1de52e",
+    "url": "/eyadatak/static/media/error-404.bee7ecf0.svg"
+  },
+  {
+    "revision": "2ef8ba3410dcc71578a880e7064acd7a",
+    "url": "/eyadatak/static/media/fa-brands-400.2ef8ba34.woff"
+  },
+  {
+    "revision": "5bf145531213545e03ff41cd27df7d2b",
+    "url": "/eyadatak/static/media/fa-brands-400.5bf14553.svg"
+  },
+  {
+    "revision": "5e2f92123d241cabecf0b289b9b08d4a",
+    "url": "/eyadatak/static/media/fa-brands-400.5e2f9212.woff2"
+  },
+  {
+    "revision": "98b6db59be947f563350d2284fc9ea36",
+    "url": "/eyadatak/static/media/fa-brands-400.98b6db59.ttf"
+  },
+  {
+    "revision": "a7b95dbdd87e0c809570affaf366a434",
+    "url": "/eyadatak/static/media/fa-brands-400.a7b95dbd.eot"
+  },
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "/eyadatak/static/media/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "/eyadatak/static/media/fa-solid-900.46e7cec6.eot"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "/eyadatak/static/media/fa-solid-900.49279363.svg"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "/eyadatak/static/media/fa-solid-900.a7140145.woff"
+  },
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "/eyadatak/static/media/fa-solid-900.ff8d9f8a.ttf"
+  },
+  {
+    "revision": "1aae4c510a34d8f643ee61ae05a27378",
+    "url": "/eyadatak/static/media/facebook-icon.1aae4c51.svg"
+  },
+  {
+    "revision": "10a48f597111bf1ebe8262cb15d1dc92",
+    "url": "/eyadatak/static/media/fawry.10a48f59.png"
+  },
+  {
+    "revision": "b58629af6fd4c249350eb8c8c1d680b5",
+    "url": "/eyadatak/static/media/file.b58629af.svg"
+  },
+  {
+    "revision": "d951ddfbf3c4da57298836b377e89ea6",
+    "url": "/eyadatak/static/media/help.d951ddfb.svg"
+  },
+  {
+    "revision": "0a0337160a37a3db2d28f7c252bc9841",
+    "url": "/eyadatak/static/media/instagram-icon.0a033716.svg"
+  },
+  {
+    "revision": "250a01b28aa7eb7ce0cefb7381d98a1f",
+    "url": "/eyadatak/static/media/left-arrow.250a01b2.svg"
+  },
+  {
+    "revision": "315663999abb477e02a6e3e2a23e9957",
+    "url": "/eyadatak/static/media/login-shape.31566399.svg"
+  },
+  {
+    "revision": "a13fd722d52fd8c3f7e8fd6462720caa",
+    "url": "/eyadatak/static/media/login-shape2.a13fd722.svg"
+  },
+  {
+    "revision": "7e811f81c123e063bfdde9cc31998745",
+    "url": "/eyadatak/static/media/logo-white.7e811f81.svg"
+  },
+  {
+    "revision": "46b0398190e3d1dc7efa784e4512c718",
+    "url": "/eyadatak/static/media/logout.46b03981.svg"
+  },
+  {
+    "revision": "1e8e1a254514fe7da13ecbf1e635cdef",
+    "url": "/eyadatak/static/media/magnifying-glass.1e8e1a25.svg"
+  },
+  {
+    "revision": "23c6b7f72cf44d2549adf4bd3caf6c9e",
+    "url": "/eyadatak/static/media/next-arrow.23c6b7f7.svg"
+  },
+  {
+    "revision": "9ec842aef01354578a969a609aaac3e3",
+    "url": "/eyadatak/static/media/note.9ec842ae.svg"
+  },
+  {
+    "revision": "67a92a4e4003f4bc6d8adc5f8b081a2a",
+    "url": "/eyadatak/static/media/notosans-bold-webfont.67a92a4e.woff"
+  },
+  {
+    "revision": "b6093f167f39fa1b9544288e4f33939e",
+    "url": "/eyadatak/static/media/notosans-bold-webfont.b6093f16.woff2"
+  },
+  {
+    "revision": "8715ac69e5501a37ecd6f8dd1a218dc0",
+    "url": "/eyadatak/static/media/notosans-regular-webfont.8715ac69.woff"
+  },
+  {
+    "revision": "c95ce4332a6ec9e3620a3b6dad815ede",
+    "url": "/eyadatak/static/media/notosans-regular-webfont.c95ce433.woff2"
+  },
+  {
+    "revision": "8336055b1586f06490192d3105a175fa",
+    "url": "/eyadatak/static/media/patient-login.8336055b.png"
+  },
+  {
+    "revision": "96cc8babec76ee229f15e25fccbd8852",
+    "url": "/eyadatak/static/media/prev-arrow.96cc8bab.svg"
+  },
+  {
+    "revision": "fde98d39d3ae38f5a47f0804a4139196",
+    "url": "/eyadatak/static/media/profile-pic01.fde98d39.svg"
+  },
+  {
+    "revision": "60bb08df0ad7a3f36790be6c2a5c9b1f",
+    "url": "/eyadatak/static/media/roboto-bold-webfont.60bb08df.woff2"
+  },
+  {
+    "revision": "d6bdf1986fea458868046f5392a98af8",
+    "url": "/eyadatak/static/media/roboto-bold-webfont.d6bdf198.woff"
+  },
+  {
+    "revision": "21c233ac5b915c4b3af56e77d908a9b0",
+    "url": "/eyadatak/static/media/roboto-light-webfont.21c233ac.woff"
+  },
+  {
+    "revision": "a0aae9d4c92fba6cdc3f2f5716e184ef",
+    "url": "/eyadatak/static/media/roboto-light-webfont.a0aae9d4.woff2"
+  },
+  {
+    "revision": "00d0df88ab10e250e223dbb435cb2543",
+    "url": "/eyadatak/static/media/roboto-medium-webfont.00d0df88.woff2"
+  },
+  {
+    "revision": "588094b0827f41b5ee0aa5615cec1dcc",
+    "url": "/eyadatak/static/media/roboto-medium-webfont.588094b0.woff"
+  },
+  {
+    "revision": "4fc1cac3a7bd3c019625964b23cec95c",
+    "url": "/eyadatak/static/media/roboto-regular-webfont.4fc1cac3.woff2"
+  },
+  {
+    "revision": "8d602fd0324f0d4414b63b2c7bc725e3",
+    "url": "/eyadatak/static/media/roboto-regular-webfont.8d602fd0.woff"
+  },
+  {
+    "revision": "489073b20dd3fa77479a2a1c54eba792",
+    "url": "/eyadatak/static/media/select-arrow.489073b2.svg"
+  },
+  {
+    "revision": "f845f89f658e77006d6801981520ce1c",
+    "url": "/eyadatak/static/media/settings-icon.f845f89f.svg"
+  },
+  {
+    "revision": "b11d6add48fa7cb72ad2c1d0aa674ed4",
+    "url": "/eyadatak/static/media/sort.b11d6add.svg"
+  },
+  {
+    "revision": "15d20519c3c2e452771ae1691d52183b",
+    "url": "/eyadatak/static/media/twitter-icon.15d20519.svg"
+  },
+  {
+    "revision": "d37fd2264bdf3d133bb7723aa5489325",
+    "url": "/eyadatak/static/media/user-info.d37fd226.svg"
+  },
+  {
+    "revision": "04f1ad6010dbaadff932b47972f4c551",
+    "url": "/eyadatak/static/media/wallet-icon.04f1ad60.svg"
+  },
+  {
+    "revision": "0389c219ceb477adb932023cdd23d8c7",
+    "url": "/eyadatak/static/media/youtube-icon.0389c219.svg"
+  }
+]);
